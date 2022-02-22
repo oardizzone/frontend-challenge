bb7bf54686be88c393a6294e393879b3bbc738c7
oliverardizzone <oliverardizzone@gmail.com>
2022-02-22 10:58:32 +0100

    implemented date formatting fucnction

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import Form from './components/Form/Form';
 import Card from './components/Card/Card';
 import './app.scss';
 
+const formatDate = (dateObj) => {
+    const year = dateObj.getFullYear();
+    const month = dateObj.getMonth() + 1;
+    const day = dateObj.getDate();
+    const hours = dateObj.getHours();
+    const mins = dateObj.getMinutes();
+
+    return `${year}. ${month}. ${day} - ${hours}:${mins}`;
+};
+
 const testOrder = {
     id: 0,
-    date: Date.now(),
+    date: formatDate(new Date()),
     packagePoint: '1. számú posta - Pécs Légszeszgyár u 12./B',
     weight: 100
 };
 
 function App() {
+    console.log(formatDate(new Date()));
     return (
         <div className="container">
             <h1 className="header">Megrendelő űrlap</h1>
